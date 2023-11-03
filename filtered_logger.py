b04391411da743da24c3d2c6a3420b6b1538ae01
Benonking <benonkighoma@gmail.com>
2023-11-03 15:37:01 +0300

    Regex-ing

---
+++ filtered_logger.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+
+'''
+import re
+from typing import List
+
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> None:
+    '''
+    function to obscate a property
+    Args:
+        fileds: list of strings that contain property files to obscate
+        redaction: string representing by what the filed will be replaced
+        message: log line
+        separator: a string representing by which character
+                    is separating all fields in the log line
+    TECNIC USE re.sub
+    '''
+    pattern = '|'.join(fields)
+    return re.sub(f'({pattern})=[^{separator}]*', f'\\1={redaction}', message)
