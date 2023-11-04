af8aeb8c0f443b1af90120267e7fcfdc2a568475
Benonking <benonkighoma@gmail.com>
2023-11-04 11:49:14 +0300

    refactor get_db function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -81,9 +81,9 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     connect to decure db
     '''
     db_config = {
-        "user": os.getenv("PERSONAL_DATA_DB_USERNAME"),
-        "password": os.getenv("PERSONAL_DATA_DB_PASSWORD"),
-        "host": os.getenv("PERSONAL_DATA_DB_HOST"),
+        "user": os.getenv("PERSONAL_DATA_DB_USERNAME") or "root",
+        "password": os.getenv("PERSONAL_DATA_DB_PASSWORD") or "",
+        "host": os.getenv("PERSONAL_DATA_DB_HOST") or "localhost",
         "database": os.getenv("PERSONAL_DATA_DB_NAME")
         }
     conn = mysql.connector.connect(**db_config)
