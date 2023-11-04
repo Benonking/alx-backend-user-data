fe92ae910978e6b6a1c5c9c56dece1882dc7d171
Benonking <benonkighoma@gmail.com>
2023-11-04 16:30:31 +0300

    hash pasword

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,11 +3,13 @@
 Hash password
 '''
 import bcrypt
+
+
 def hash_password(password: str) -> str:
-	'''
-	hash user password
-	'''
-	
-	salt = bcrypt.gensalt()
-	hashed_paswd = bcrypt.hashpw(password.encode('utf-8'), salt)
-	return hashed_paswd
+    '''
+    hash user password
+    '''
+
+    salt = bcrypt.gensalt()
+    hashed_paswd = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_paswd
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -5,11 +5,13 @@ Hash password
 import bcrypt
 
 
+
+
 def hash_password(password: str) -> str:
     '''
     hash user password
     '''
-
     salt = bcrypt.gensalt()
     hashed_paswd = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_paswd
+
