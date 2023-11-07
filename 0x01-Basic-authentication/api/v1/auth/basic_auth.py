781dde1b5ba6c8199f91dcd8efc042ed4b693e32
Benonking <benonkighoma@gmail.com>
2023-11-07 14:58:31 +0300

    base 64 decode

--- 0x01-Basic-authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic-authentication/api/v1/auth/basic_auth.py
@@ -2,11 +2,45 @@
 '''
 class BasicAuth
 '''
-
+import base64
 from api.v1.auth.auth import Auth
 
+
 class BasicAuth(Auth):
-	'''
-	'''
-	
-	
+    '''
+    Implement request Basic Authentication
+    '''
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        '''
+                return Base64 part of the Authorix=zation
+                header for Basic Authentication
+        '''
+        if authorization_header is None or \
+                not isinstance(authorization_header, str):
+            return None
+        if authorization_header.startswith('Basic '):
+            # return value after basic
+            return authorization_header[len('Basic '):]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        '''
+        return decoded value of Base64string
+        '''
+        if base64_authorization_header is None or \
+                not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            # attempt to decode the base64 string
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            # Decode the bytes to a UTF-8 string and return it
+            return decoded_bytes.decode('utf-8',)
+
+        except Exception as e:
+            # Handle potential decoding errors
+            return None
