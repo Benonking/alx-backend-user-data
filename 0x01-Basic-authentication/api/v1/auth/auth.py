16d97cd90d1c3db9b82c7ac193bae1328c07e559
Benonking <benonkighoma@gmail.com>
2023-11-07 11:27:08 +0300

    define routes that do not need authentication

--- 0x01-Basic-authentication/api/v1/auth/auth.py
+++ 0x01-Basic-authentication/api/v1/auth/auth.py
@@ -7,26 +7,32 @@ from typing import List, TypeVar
 
 
 class Auth:
-        '''
-        '''
+    '''
+    '''
 
-        def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-            '''
-            define paths that do not need authentication
-            '''
-            if path is not None:
-                if path in excluded_paths:
-                    
-                    return False
-            if path + '/' in excluded_paths or path[:-1] in excluded_paths:
-                return False
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''
+        define paths that do not need authentication
+        '''
+        if path is None or excluded_paths is None or not excluded_paths:
             return True
 
-        def authorization_header(self, request=None) ->str:
-                '''
-                '''
-                return None
-        def current_user(self, request=None) -> TypeVar('User'):
-                '''
-                '''
-                return None
+        # Ensure that path ends with a trailing slash
+        if not path.endswith('/'):
+            path += '/'
+
+        # Check if path is in the list of excluded_paths
+        if path in excluded_paths:
+            return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        '''
+        '''
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''
+        '''
+        return None
