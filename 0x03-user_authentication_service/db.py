0821640ec0b5df4846aa538fc24300cbff4b596d
Benonking <benonkighoma@gmail.com>
2023-11-17 11:16:54 +0300

    change find_user by method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """DB module
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
@@ -64,29 +64,19 @@ class DB:
             NoResultFound: If no matching user is found.
             InvalidRequestError: If an invalid query argument is provided.
         """
-        try:
-            # Query the database using the provided arguments
-            user = self._session.query(User).filter_by(**kwargs).first()
-
-            # Raise NoResultFound if no user is found
-            if user is None:
-                raise NoResultFound("No Found")
-
-            return user
-        except InvalidRequestError as e:
-            # Catch and re-raise InvalidRequestError
-            raise InvalidRequestError("Invalid")
-        # try:
-        #     # query the db using provided key , value pair
-        #     user = self._session.query(User).filter_by(**kwargs).first()
-        #     if user is None:
-        #         raise NoResultFound('Not Found')
-        #     return user
-        # except InvalidRequestError:
-        #     for k in kwargs.keys():
-        #         # check if atrribute exists
-        #         if not hasattr(User, k):
-        #             raise InvalidRequestError('Invalid')
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
 
     def update_user(self, user_id: int, **kwargs: dict) -> None:
         """update a user in the database
